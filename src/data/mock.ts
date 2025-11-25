ebb4b52de0b1f2b13413b8d84eaa6c19dda65fe8
Bruno Henrique Saibert <brunosaibert@gmail.com>
2025-11-25 20:29:40 -0300

    feat: create data mock

---
+++ src/data/mock.ts
@@ -0,0 +1,18 @@
+export const MOCK_ARTISTS = [
+  { id: 1, name: 'Marshmello', image: '/images/marshmello.jpg' },
+  { id: 2, name: 'Alok', image: '/images/alok.jpg' },
+  { id: 3, name: 'Rita Ora', image: '/images/rita-ora.jpg' },
+  { id: 4, name: 'Dream Theater', image: '/images/dream-theater.jpg' },
+  { id: 5, name: 'Dua Lipa', image: '/images/dua-lipa.jpg' },
+  { id: 6, name: 'Måneskin', image: '/images/maneskin.jpg' },
+  { id: 7, name: 'Emicida', image: '/images/emicida.jpg' },
+  { id: 8, name: 'Coldplay', image: '/images/coldplay.jpg' },
+  { id: 9, name: 'Justin Bieber', image: '/images/justin-bieber.jpg' },
+  { id: 10, name: 'Luísa Sonza', image: '/images/luisa-sonza.jpg' },
+  { id: 11, name: 'Djavan', image: '/images/djavan.jpg' },
+  { id: 12, name: 'Offspring', image: '/images/offspring.jpg' },
+  { id: 13, name: 'Avril Lavigne', image: '/images/avril-lavigne.jpg' },
+  { id: 14, name: 'Black Pantera', image: '/images/black-pantera.jpg' },
+  { id: 15, name: 'Ed Sheeron', image: '/images/ed-sheeron.jpg' },
+  { id: 16, name: 'Iron Maiden', image: '/images/iron-maiden.jpg' },
+]
